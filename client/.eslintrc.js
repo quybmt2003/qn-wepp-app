27b16dad1a14a1af9dd5fd22e3c920d80916a54c
Quy Nguyen Duc <quybmt2003@gmail.com>
2021-09-13 22:02:23 +0700

    Lint fix

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
- plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     semi: ['error', 'never'],
     'react/prop-types': 0,
